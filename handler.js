46669e4dbe3bbea52603ae5a4425534c2fc6d512
Mohammad-Quanit <muhammadquanit@gmail.com>
2020-03-22 21:13:53 +0500

    Serverless rest api

---
+++ handler.js
@@ -0,0 +1,93 @@
+"use strict";
+
+const uuid = require("uuid");
+const AWS = require("aws-sdk");
+
+// comment or remove when deploying
+const dynamoDB = new AWS.DynamoDB.DocumentClient({
+  region: "localhost",
+  endpoint: "http://localhost:8000"
+});
+
+module.exports.create = (event, context, callback) => {
+  // create a note and save it into dynamoDB
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: { id: uuid.v1(), content: data.content },
+  };
+
+  dynamoDB.put(params, error => {
+    if (error) {
+      return callback(null, {
+        statusCode: error.statusCode || 500,
+        headers: { "Content-Type": "text/plain" },
+        body: `Couldn't create note. ${error.message}`
+      });
+    }
+    callback(null, { statusCode: 200, body: JSON.stringify(params.Item) })
+  });
+
+  
+};
+
+module.exports.getOne = (event, context, callback) => {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: { id: event.pathParameters.id }
+  }
+
+  dynamoDB.get(params, (error, result) => {
+    if (error) {
+      return callback(null, {
+        statusCode: error.statusCode || 500,
+        headers: { "Content-Type": "text/plain" },
+        body: `Couldn't create note. ${error.message}`
+      });
+    }
+    callback(null, { statusCode: 200, body: JSON.stringify(result.Item) })
+  })
+};
+
+module.exports.getAll = (event, context, callback) => {
+  const params = { TableName: process.env.DYNAMODB_TABLE, }
+  dynamoDB.scan(params, (error, result) => {
+    if (error) {
+      return callback(null, {
+        statusCode: error.statusCode || 500,
+        headers: { "Content-Type": "text/plain" },
+        body: `Couldn't create note. ${error.message}`
+      });
+    }
+    callback(null, { statusCode: 200, body: JSON.stringify(result.Items) })
+  })
+};
+
+module.exports.update = (event, context, callback) => {
+  const data = JSON.parse(event.body)
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: { id: event.pathParameters.id },
+    ExpressionAttributeValues: { ":content": data.content },
+    UpdateExpression: "SET content = :content",
+    ReturnValues: 'ALL_NEW'
+  }
+  dynamoDB.update(params, (error, result) => {
+    if (error) {
+      return callback(null, {
+        statusCode: error.statusCode || 500,
+        headers: { "Content-Type": "text/plain" },
+        body: `Couldn't update note. ${error.message}`
+      });
+    }
+    callback(null, { statusCode: 200, body: JSON.stringify(result.Attributes) })
+  })
+};
+
+module.exports.delete = async event => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify("deleting a note.")
+  };
+  return response;
+};
